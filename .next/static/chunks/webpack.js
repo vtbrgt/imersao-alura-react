5d94255591b48dd833fd404137c69deabd463fc9
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-11-10 20:07:30 -0300

    final aula 4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99ec0ec0a84c6040"; }
+/******/ 		__webpack_require__.h = function() { return "21b071b8fc2f9e4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import { StyledRegisterVideo } from './styles';
 
+// Custom Hook
+function useForm(propsDoForm) {
+  const [values, setValues] = React.useState(propsDoForm.initialValues);
+  return {
+    values,
+    handleChange: (event) => {
+      const value = event.target.value;
+      const name = event.target.name;
+      setValues({
+        ...values,
+        [name]: value,
+      });
+    },
+    clearForm: () => {
+      setValues({});
+    },
+  };
+}
+
 export default function RegisterVideo() {
+  const formCadastro = useForm({
+    initialValues: {
+      titulo: 'Nico Robin - Ennies Lobbie',
+      url: 'https://www.youtube.com',
+    },
+  });
   const [formVisivel, setFormVisivel] = React.useState(false);
+
   return (
     <StyledRegisterVideo>
       <button className="add-video" onClick={() => setFormVisivel(true)}>
         +
       </button>
       {formVisivel ? (
-        <form>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            setFormVisivel(false);
+            formCadastro.clearForm();
+          }}
+        >
           <div>
             <button
+              type="button"
               className="close-modal"
               onClick={() => setFormVisivel(false)}
             >
               X
             </button>
-            <input placeholder="Título do vídeo" />
-            <input placeholder="URL" />
+            <input
+              placeholder="Título do vídeo"
+              name="titulo"
+              value={formCadastro.values.titulo}
+              onChange={formCadastro.handleChange}
+            />
+            <input
+              placeholder="URL"
+              name="url"
+              value={formCadastro.values.url}
+              onChange={formCadastro.handleChange}
+            />
             <button type="submit">Cadastrar</button>
           </div>
         </form>
